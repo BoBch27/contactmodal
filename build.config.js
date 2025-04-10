c82f3c061f2ebff84e75f8f432705f1a5b27faf6
Bobby Donev <donev.bobby@gmail.com>
2025-04-10 04:57:53 +0200

    Added css minifying

--- build.config.js
+++ build.config.js
@@ -1,11 +1,24 @@
-import { build } from 'esbuild';
+import { transform, build } from 'esbuild';
+import { readFile } from "fs/promises";
+
+const CSSMinifyPlugin = {
+    name: "CSSMinifyPlugin",
+    setup(build) {
+        build.onLoad({ filter: /\.css$/ }, async (args) => {
+            const f = await readFile(args.path);
+            const css = await transform(f, { loader: "css", minify: true });
+            return { loader: "text", contents: css.code };
+        });
+    }
+};
 
 build({
     entryPoints: ['src/contactmodal.js'],
     bundle: true,
     outfile: 'dist/contactmodal.min.js',
     minify: true,
-    loader: { '.html': 'text', '.css': 'text' },
+    loader: { '.html': 'text' },
     format: 'iife',
-    target: 'es6'
+    target: 'es6',
+    plugins: [CSSMinifyPlugin],
 }).catch(() => process.exit(1));
